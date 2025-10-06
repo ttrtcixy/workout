1a507394afb13892c4c6a67ebe710f661575f6c0
ttrtcixy <ttrtcixy@gmail.com>
2025-10-07 01:11:02 +0400

    feat: add new env variable parsing

--- internal/config/db.go
+++ internal/config/db.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 //type DBConfig interface {
@@ -10,17 +11,28 @@ import (
 //}
 
 type DB struct {
-	dsn string
+	dsn            string
+	connectTimeout time.Duration
 }
 
 func (c *Config) LoadDbConfig(fErr *errEnvVariableNotFound) {
 	const op = "Config.LoadDbConfig"
 	cfg := &DB{}
 
-	if value, ok := os.LookupEnv("DB_URL"); ok {
-		cfg.dsn = value
+	if env, ok := os.LookupEnv("DB_URL"); ok {
+		cfg.dsn = env
 	} else {
 		fErr.Add(fmt.Errorf("%s: env variable 'DB_URL' is not set", op))
+	}
+
+	if env, ok := os.LookupEnv("DB_CONNECT_TIMEOUT"); ok {
+		t, err := time.ParseDuration(env)
+		if err != nil {
+			fErr.Add(fmt.Errorf("%s: env variable 'DB_CONNECT_TIMEOUT' bad format", op))
+		}
+		cfg.connectTimeout = t
+	} else {
+		fErr.Add(fmt.Errorf("%s: env variable 'DB_CONNECT_TIMEOUT' is not set", op))
 	}
 
 	c.DB = cfg
@@ -28,4 +40,8 @@ func (c *Config) LoadDbConfig(fErr *errEnvVariableNotFound) {
 
 func (cfg *DB) DSN() string {
 	return cfg.dsn
+}
+
+func (cfg *DB) ConnectTimeout() time.Duration {
+	return cfg.connectTimeout
 }
